dd06323d86c9a16724958098b61c05ee9e06367e
zenitaxians <neziri.zenit@vinci-energies.net>
2023-04-07 22:18:57 +0200

    Initial commit

---
+++ src/main/java/com/luv2code/ecommerce/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.luv2code.ecommerce.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+//@RequestMapping(value = "/product")
+
+public class ProductController {
+
+    @GetMapping("/work")
+    private String sayHello(){
+        return "HELLO WORLD!";
+    }
+
+    @GetMapping("/job")
+    private String sayHi(){
+        return "HI WORLD!";
+    }
+    @GetMapping("/kia")
+    private String sayKIA(){
+        return "KIA WORLD!";
+    }
+
+
+
+
+
+}
---
+++ src/main/java/com/luv2code/ecommerce/dao/ProductCategoryRepository.java
@@ -0,0 +1,13 @@
+package com.luv2code.ecommerce.dao;
+
+import com.luv2code.ecommerce.entity.ProductCategory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+/*CORS - Cross Origin Resource Sharing is a standard
+ that manages communication between 2 or multiple domains*/
+@CrossOrigin("http://localhost:4200")
+@RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
+}
