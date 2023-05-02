e13a46114fe9bf585c1d9762db079f5dc9600ad7
zenitaxians <neziri.zenit@vinci-energies.net>
2023-05-02 17:05:35 +0200

    fixed bug

--- src/main/java/com/luv2code/ecommerce/SpringJenkinsApplication.java
+++ src/main/java/com/luv2code/ecommerce/SpringJenkinsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringJenkinsApplication {//testets
 
-	public static main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Hello world!");//teste teste .teste
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
