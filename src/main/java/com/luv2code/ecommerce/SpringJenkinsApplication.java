251df188a380428827b6fa9912a23b0806043bdc
zenitaxians <neziri.zenit@vinci-energies.net>
2023-05-02 14:58:46 +0200

    resolved error

--- src/main/java/com/luv2code/ecommerce/SpringJenkinsApplication.java
+++ src/main/java/com/luv2code/ecommerce/SpringJenkinsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringJenkinsApplication {//testets
 
-	public  void main(String[] args) {
+	public static void main(String[] args) {
 		System.out.println("Hello world!");
 		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
