ec51b7b6fff7434d983f8f1b933bfe07c6bddb63
zenitaxians <neziri.zenit@vinci-energies.net>
2023-05-02 14:16:13 +0200

    some changes

--- src/main/java/com/luv2code/ecommerce/SpringJenkinsApplication.java
+++ src/main/java/com/luv2code/ecommerce/SpringJenkinsApplication.java
@@ -5,10 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class SpringBootEcommerceApplication {//testets
+public class SpringJenkinsApplication {//testets
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootEcommerceApplication.class, args);
+		System.out.println("Hello world!");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
 	}
 
 }
