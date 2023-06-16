c0edb53000b789d176161a66687e05f19d922634
zenitaxians <neziri.zenit@vinci-energies.net>
2023-06-16 08:47:32 +0200

    test zenit

--- src/main/java/com/luv2code/ecommerce/SpringBootApplication.java
+++ src/main/java/com/luv2code/ecommerce/SpringBootApplication.java
@@ -8,5 +8,6 @@ public class SpringBootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootApplication.class, args);
+		System.out.println("TESTZZZZZZZZZZZZ");
 	}
 }
